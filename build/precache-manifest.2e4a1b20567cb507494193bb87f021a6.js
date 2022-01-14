4d562f14bb98ed6bc19d68608dbd77a7bc0b0d37
heyiminhye <heyiminhye@gmail.com>
2022-01-14 18:34:21 +0900

    Add: build folder

---
+++ build/precache-manifest.2e4a1b20567cb507494193bb87f021a6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ce05284d8a2305d81ea414693e2049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19758412be08d10900dc",
+    "url": "/static/js/2.33130830.chunk.js"
+  },
+  {
+    "revision": "53bd9c106afeb6121947",
+    "url": "/static/js/main.4744e4be.chunk.js"
+  },
+  {
+    "revision": "e9b3aa2ab3a81ef36579",
+    "url": "/static/js/runtime-main.075ca6cb.js"
+  }
+]);
